70d328092c6f878f18ada380a3d9b291c435f55f
stennn <stennn@msn.com>
2024-01-25 21:40:28 +0200

    add completed tasks

--- todo_main/views.py
+++ todo_main/views.py
@@ -6,7 +6,10 @@ from todos.models import Task
 #home
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by("-updated_at")
+    completed_tasks = Task.objects.filter(is_completed=True).order_by("updated_at")
+
     context = {
         "tasks": tasks,
+        "completed_tasks": completed_tasks,
     }
     return render(request, "home.html", context)
