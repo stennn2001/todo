14c87f55442655a78ac0f31740e76fa8aba37909
stennn <stennn@msn.com>
2024-01-25 21:24:14 +0200

    first commit

---
+++ todo_main/views.py
@@ -0,0 +1,12 @@
+
+from django.shortcuts import render
+
+from todos.models import Task
+
+
+def home(request):
+    tasks = Task.objects.filter(is_completed=False).order_by("-updated_at")
+    context = {
+        "tasks": tasks,
+    }
+    return render(request, "home.html", context)
